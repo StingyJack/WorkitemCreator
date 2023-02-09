d82b0a5b5cdd1263dde15e43345bac997fe9f96f
Andrew Stanton <astanton1978@hotmail.com>
2023-02-09 08:54:07 -0500

    Add project files.

---
+++ WorkitemCreator/WorkitemTemplates.cs
@@ -0,0 +1,29 @@
+﻿namespace WorkitemCreator
+{
+    using System;
+    using System.Collections.Generic;
+
+    class WorkitemTemplates
+    {
+        public List<WorkitemTemplate> Templates { get; set; }
+        public string ServiceUrl { get; set; }
+    }
+
+
+    enum WorkitemType
+    {
+        Unknown,
+        UserStory,
+        Task
+    }
+
+    class WorkitemTemplate
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public WorkitemType WorkitemType { get; set; }
+        public List<WorkitemTemplate> Children { get; set; }
+        public Dictionary<string, string> Properties { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+    }
+}
